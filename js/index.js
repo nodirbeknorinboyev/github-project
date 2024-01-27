30bbe77ef8953f4a187f0748c0d7282c09b77859
nodir2208 <norinboyevnortoy544@gmail.com>
2024-01-27 21:04:07 +0500

    init

---
+++ js/index.js
@@ -0,0 +1,44 @@
+//Bool-1
+//const a = 5
+//const b = 5
+
+
+//function beg(a, b) {
+  // return a === b
+//}
+
+//console.log(beg(a ,b))
+
+//Bool-2
+
+//const a = 7;
+//const b = 9;
+
+
+//function beg(a, b) {
+   //return a === b
+//}
+//console.log(beg(a, b));
+
+//Bool-3
+
+//const b = -6;
+ 
+
+//function beg(b) {
+  // return b > 0
+//}
+//console.log(beg(b));
+
+//Bool-4
+
+//const a = 3;
+
+
+//function beg(a) {
+   //return a < 0
+//}
+//console.log(beg(a));
+
+//Bool-7
+
